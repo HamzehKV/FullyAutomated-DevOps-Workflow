81d072542beeb01176bc3dcb55c66ebee51ca88d
Hamzeh Karbasiyan <karbasiyan.hamzeh@gmail.com>
2020-08-07 01:31:44 +0200

    Added main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+provider "azurerm" {
+  version = "2.5.0"
+  features {}
+}
+
+resource "azurerm_resource_group" "tfrg_weatherapi" {
+  name     = "TFRG_weatherapi"
+  location = "westeurope"
+}
+
+resource "azurerm_container_group" "tfcg_weatherapi" {
+  name                = "TFCG_weatherapi"
+  location            = azurerm_resource_group.tfrg_weatherapi.location
+  resource_group_name = azurerm_resource_group.tfrg_weatherapi.name
+
+  ip_address_type = "public"
+  dns_name_label  = "hamzehwa"
+  os_type         = "linux"
+  container {
+    name   = "weatherapi"
+    image  = "hmzkv88/weatherapi"
+    cpu    = "1"
+    memory = "1"
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+
+  }
+}
